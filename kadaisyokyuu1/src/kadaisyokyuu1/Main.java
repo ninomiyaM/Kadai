a94d93dd84a85157df2907771f3c18d92d76f84b
Meguru Ninomiya <m.ninomiya@hr-next.co.jp>
2020-08-28 11:20:03 +0900

    初回コミット

---
+++ kadaisyokyuu1/src/kadaisyokyuu1/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package kadaisyokyuu1;
+
+import java.util.*;
+
+/**
+ * @author ninomiya
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception{
+		// TODO 自動生成されたメソッド・スタブ
+			int num = 2; 
+			String name[] = {"ando" , "noda"};
+			
+			for (int i = 0; i < num; i++) {
+				
+			}
+			
+			
+			
+	}
+}
