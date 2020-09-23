d525f05107bb90a2eb9528cb67c8283660417e60
Meguru Ninomiya <m.ninomiya@hr-next.co.jp>
2020-09-23 14:06:25 +0900

    2回目

--- kadaisyokyuu2/src/kadaisyokyuu2/Main.java
+++ kadaisyokyuu2/src/kadaisyokyuu2/Main.java
@@ -3,30 +3,34 @@ package kadaisyokyuu2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); //嫌いな数字
+		String n = sc.nextLine(); //嫌いな数字
 		sc.nextLine();
 		String m = sc.nextLine(); //部屋数
 		sc.nextLine();
-		List<Integer> list = new ArrayList<Integer>();
-		List<Integer> s = new ArrayList<Integer>();
+		List<String> list = new ArrayList<String>();
+		List<String> s = new ArrayList<String>();
 		while (sc.hasNext()) {
-			list.add(sc.nextInt());
+			list.add(sc.nextLine());
 		}
 		
-		if( m != null) {
 			int m1 = Integer.parseInt(m); //部屋数を数値化
-			int r = sc.nextInt(); //部屋番号
+			String r = sc.nextLine(); //部屋番号
 			
-			list.stream()
-				.filter(a -> (list).startsWith(n))
-				.collect(s.toList);
+			for (int i = 0; i <= m1; i++) {
+				
+				
+				list.stream()
+					.filter(( a -> list.contains(n)))
+					.collect(Collectors.toList());
 			
+					
 			
 			}
 			
